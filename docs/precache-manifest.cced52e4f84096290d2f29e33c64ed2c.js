ede546d79053923ad5fe01b79cd6fa46ef12e256
Arthur <Strevitz@github.com>
2020-07-15 14:09:22 +0200

    commit

---
+++ docs/precache-manifest.cced52e4f84096290d2f29e33c64ed2c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "162a8ff5f600a5737c692cc347d6e6d4",
+    "url": "/weather-app-react/index.html"
+  },
+  {
+    "revision": "2e23df28491b839844f4",
+    "url": "/weather-app-react/static/css/main.80a87f59.chunk.css"
+  },
+  {
+    "revision": "56d89a8aba35dabf02b0",
+    "url": "/weather-app-react/static/js/2.b08500a9.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/weather-app-react/static/js/2.b08500a9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2e23df28491b839844f4",
+    "url": "/weather-app-react/static/js/main.a0f47398.chunk.js"
+  },
+  {
+    "revision": "0965184a592c71c34fe2",
+    "url": "/weather-app-react/static/js/runtime-main.0d456eba.js"
+  }
+]);
